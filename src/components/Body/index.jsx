3e2c322dd49756deebf5dfce779820dc233d32de
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 01:23:10 -0700

    Create Body component file

---
+++ src/components/Body/index.jsx
@@ -0,0 +1,11 @@
+import './body.css';
+
+function Body() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Body
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+// style
+import './header.css';
 
 function Header() {
     return (
