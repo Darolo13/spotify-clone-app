eb168e7c82fa0a5108bfbe8e17684ea049d6acf7
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 02:06:57 -0700

    left Header section

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useStateValue } from "../StateProvider";
+
+// material-ui
+import SearchIcon from "@material-ui/icons/Search";
 
 // style
 import "./header.css";
@@ -9,7 +12,13 @@ function Header({ spotify }) {
 
   return (
     <div className="header">
-      <div className="header__left"></div>
+      <div className="header__left">
+        <SearchIcon />
+        <input
+          placeholder="Search for Artists, Songs, or Podcasts"
+          type="text"
+        />
+      </div>
       <div className="header__right"></div>
     </div>
   );
