b4cea74e886b2fccad14ffda237c41a83d46e1d1
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 02:30:06 -0700

    Right Header Section

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,23 +3,27 @@ import { useStateValue } from "../StateProvider";
 
 // material-ui
 import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
 
 // style
 import "./header.css";
 
 function Header({ spotify }) {
-  const [{ user }, dispatch] = useStateValue();
+    const [{ user }, dispatch] = useStateValue();
 
   return (
     <div className="header">
       <div className="header__left">
         <SearchIcon />
         <input
-          placeholder="Search for Artists, Songs, or Podcasts"
+          placeholder="Search for Artists, Songs, or Podcasts "
           type="text"
         />
       </div>
-      <div className="header__right"></div>
+      <div className="header__right">
+        <Avatar alt={user?.display_name} src={user?.images[0]?.url} />
+        <h4>{user?.display_name}</h4>
+      </div>
     </div>
   );
 }
