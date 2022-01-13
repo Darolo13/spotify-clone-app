7160d7f5937de2980677ad288baa88d83545d46f
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 00:24:28 -0700

    Create StateProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+// spotify api
+import { getTokenFromResponse } from "./spotify";
+
 // components
 import Login from "./components/Login";
 
 function App() {
+
   return (
     <div className="app">
       <Login />
