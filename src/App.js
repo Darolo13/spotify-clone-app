44ec8f9072590e92753d29c4b188118644d8fbf5
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 00:51:20 -0700

    Set up token

--- src/App.js
+++ src/App.js
@@ -1,14 +1,74 @@
-// spotify api
+import React, { useEffect } from "react";
+
+// spotify token
 import { getTokenFromResponse } from "./spotify";
+// spotify web API
+import SpotifyWebApi from "spotify-web-api-js";
+
+// State Provider
+import { useStateValue } from "./components/StateProvider";
 
 // components
 import Login from "./components/Login";
 
+const s = new SpotifyWebApi();
+
 function App() {
+  const [{ token }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // set token
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+
+    let _token = hash.access_token;
+
+    if (_token) {
+      s.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      s.getPlaylist("37i9dQZF1E8BcQf6zjTlm0").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
 
   return (
     <div className="app">
-      <Login />
+      {!token && <Login />}
     </div>
   );
 }
