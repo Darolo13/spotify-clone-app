c0e9b82d85111abee4c393f8b1c5027a3a2d022a
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 03:43:11 -0700

    Style Sidebar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,12 +5,20 @@ import { getTokenFromResponse } from "../../spotify";
 import { useStateValue } from "../StateProvider";
 
 // material-ui
-// import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+
+// component
+import SideBarOption from "./SideBarOption";
 
 // style
 import "./sidebar.css";
 
 function Sidebar() {
+
+    const [{ playlists }] = useStateValue();
+
   return (
     <div className="sidebar">
       <img
@@ -18,6 +26,15 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+      <SideBarOption Icon={HomeIcon} option="Home" />
+      <SideBarOption Icon={SearchIcon} option="Search" />
+      <SideBarOption Icon={LibraryMusicIcon} option="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      {playlists?.items?.map((playlist) => (
+          <SideBarOption option={playlist.name} />
+      ))}
     </div>
   );
 }
