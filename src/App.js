d1aa95f2d9ea632cb1b0635f3006bfcc94c2ca4a
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 00:05:03 -0700

    Set up spotify.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Login from "./components/Login";
 function App() {
   return (
     <div className="app">
-      <h1>Spotify Clone App</h1>
-
-      {/* Spotify Logo */}
-      {/* Login with spotify button */}
       <Login />
     </div>
   );
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,14 +1,16 @@
-import React from "react";
+import { loginUrl } from '../../spotify';
+import './login.css'
 
 function Login() {
   return (
     <div className="login">
-      <h1>Login Page</h1>
       <img
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
+
       {/* Login with spotify button */}
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
