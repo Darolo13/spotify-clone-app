a0eed4ebecd79c5a9e1f176a3af67c171aca5330
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 01:49:52 -0700

    Render Body component in Player component

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,10 +1,15 @@
+// components
+import Body from '../Body';
+
 // style
 import "./player.css";
 
-function Player() {
+function Player({ spotify }) {
   return (
     <div className="player">
-      <div className="player__body"></div>
+      <div className="player__body">
+        <Body spotify={spotify} />
+      </div>
     </div>
   );
 }
