e73442801b5872f8cbd3b6f13955eb9e948ebc8e
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 18:40:28 -0700

    SongRow info section

--- src/components/SongRow/index.jsx
+++ src/components/SongRow/index.jsx
@@ -2,9 +2,18 @@
 import "./songrow.css";
 
 function SongRow({ track, playSong }) {
-  return <div className="songRow" onClick={() => playSong(track.id)}>
+  return (
+    <div className="songRow" onClick={() => playSong(track.id)}>
       <img className="songRow__album" src={track.album.images[0].url} alt="" />
-  </div>;
+      <div className="songRow__info">
+        <h1>{track.name}</h1>
+        <p>
+          {track.artists.map((artist) => artist.name).join(", ")} -{" "}
+          {track.album.name}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default SongRow;
