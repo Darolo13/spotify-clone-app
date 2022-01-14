6131d53cd08421490a680ae1e8d5aac360252352
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2022-01-13 19:07:59 -0700

    Footer left section

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,34 @@
+// component
+import useStateValue from "../StateProvider";
+
 // style
 import "./footer.css";
 
-import React from 'react'
+function Footer({ spotify }) {
+  const [{ token, item, playing }, dispatch] = useStateValue();
 
-function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src={item?.album.images[0].url}
+          alt={item?.name}
+        />
+        {item ? (
+          <div className="footer__songInfo">
+            <h4>{item.name}</h4>
+            <p>{item.artists.map((artist) => artist.name).join(", ")}</p>
+          </div>
+        ) : (
+          <div className="footer__songInfo">
+            <h4>No song is playing</h4>
+            <p>...</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
